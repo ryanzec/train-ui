1ff4a696113587d86aeeabb3c6d4a0a4cd34dc2d
Ryan Zec <basire@gmail.com>
2023-12-24 13:12:55 -0500

    current progress

---
+++ packages/utils/playground.sandbox.tsx
@@ -0,0 +1,43 @@
+import * as Babel from '@babel/standalone';
+// @ts-ignore
+import babelPresetSolid from 'babel-preset-solid';
+
+import Avatar from '$/components/avatar';
+
+export default {
+  title: 'Components/Playground',
+};
+globalThis._assert = console.assert.bind(console);
+
+const code = ` 
+globalThis._assert = console.assert.bind(console)
+
+const TestComp = () => {
+  return (
+    <>
+      <div>test</div>
+    </>
+  );
+};
+
+export default TestComp;
+`;
+
+export const Default = () => {
+  const tranformed = Babel.transform(code, {
+    presets: [
+      [babelPresetSolid, { generate: 'dom', hydratable: false }],
+      ['typescript', { onlyRemoveTypeImports: true }],
+    ],
+    filename: 'index.tsx',
+  });
+
+  eval(tranformed.code);
+
+  return (
+    <div>
+      <Avatar>SJ</Avatar>
+      <Avatar src="https://avatars.githubusercontent.com/u/444206?v=4">SJ</Avatar>
+    </div>
+  );
+};
