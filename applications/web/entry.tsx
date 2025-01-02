88132275d5d9330adda7b97c6af3a4cbe62691d2
Ryan Zec <basire@gmail.com>
2025-01-02 08:30:27 -0500

    font updates

--- applications/web/entry.tsx
+++ applications/web/entry.tsx
@@ -1,9 +1,10 @@
 /* @refresh reload */
 import '$/types/solid-js';
 
-import '@fontsource/roboto';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource/open-sans';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/600.css';
+import '@fontsource/open-sans/700.css';
 
 import 'material-icons/iconfont/material-icons.css';
 import '$/styles/variables.css';
